b70b9af4228a3f2f4f3b7bc8ffe7c5d35a318a81
Ulises Rangel <uli.atx@gmail.com>
2019-03-15 13:09:18 -0500

    added javascript file and firebase script into HTML

---
+++ app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCHsRpyLVhpJZyOpZ14DssEVo60alkM8po",
+    authDomain: "ulisesproject-9cbd7.firebaseapp.com",
+    databaseURL: "https://ulisesproject-9cbd7.firebaseio.com",
+    projectId: "ulisesproject-9cbd7",
+    storageBucket: "ulisesproject-9cbd7.appspot.com",
+    messagingSenderId: "292343109983"
+  };
+  firebase.initializeApp(config);
