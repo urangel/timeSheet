25814244b11fc40e78f0b93b03b22ccd0cf7c026
Ulises Rangel <uli.atx@gmail.com>
2019-03-15 13:37:01 -0500

    some updates

--- app.js
+++ app.js
@@ -9,3 +9,43 @@
     messagingSenderId: "292343109983"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  var employeeName = "Connor";
+  var role = "Badass";
+  var startDate = "01/01/1971";
+  var monthlyRate = 
+
+  database.ref().set({
+    employeeName: employeeName,
+    role: role,
+    startDate: startDate,
+    monthlyRate: monthlyRate,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
+
+
+  $("#add-employee-btn").on("click", function(e){
+      e.preventDefault();
+
+      employeeName = $("#employee-name-input").val().trim();
+      role = $("#role-input").val().trim();
+      startDate = $("#start-input").val().trim();
+      monthlyRate = $("#rate-input").val().trim();
+
+      database.ref().push({
+        employeeName : employeeName,
+        role : role,
+        startDate : startDate,
+        monthlyRate : monthlyRate
+      })
+
+      $("#employee-name-input").val("").trim();
+      $("#role-input").val("").trim();
+      $("#start-input").val("").trim();
+      $("#rate-input").val("").trim();
+
+
+  })
+
